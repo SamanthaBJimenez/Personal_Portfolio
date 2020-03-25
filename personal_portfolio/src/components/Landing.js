a6b84397dbc78609a962810f9fdde017a1120de0
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-03-25 01:04:59 -0400

    add typist to landing.js

--- personal_portfolio/src/components/Landing.js
+++ personal_portfolio/src/components/Landing.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import Typist from 'react-typist';
+import 'react-typist/dist/Typist.css';
 import '../css/Landing.css';
 
 const Landing = () => {
     return (
         <div className='LandingDiv'>
-            <h1>Hi.</h1>
-            <h1>My name is Samantha Jimenez.</h1>
-            <h1>I'm a Fullstack Web Developer.</h1>
+        <script>
+            dist/standalone/Typist.js
+        </script>
+            <Typist>
+                Hi.
+                <Typist.Backspace count={3} delay={1500} />
+                Hey.
+                <Typist.Backspace count={4} delay={2000} />
+                Hello.
+                <Typist.Backspace count={1} delay={1500} />
+                !
+                <Typist.Delay ms={1500} />
+                <br/>
+                My name is Samantha Jimenez.
+                <Typist.Delay ms={1000} />
+                <br/>
+                I'm a Fullstack Web Developer!
+            </Typist>
         </div>
     )
 }
