6da82dc3fafe391f9ed6f188ffb6d2138f5d7786
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-03-25 16:05:47 -0400

    add skills icons and css for component

--- personal_portfolio/src/components/Skills.js
+++ personal_portfolio/src/components/Skills.js
@@ -4,7 +4,51 @@ import '../css/Skills.css';
 const Skills = () => {
     return (
         <div className='SkillsDiv'>
-            
+            <link rel='stylesheet' href='https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css'/>
+            <div className='iconDiv'>
+                <i className='devicon-javascript-plain icon'></i>
+                <p>javascript</p>
+            </div>
+            <div className='iconDiv'>
+                <i className='devicon-css3-plain icon'></i>
+                <p>css</p>
+            </div>
+            <div className='iconDiv'>
+                <i className='devicon-express-original icon'></i>
+                <p>express</p>
+            </div>
+            <div className='iconDiv'>
+                <i className='devicon-git-plain icon'></i>
+                <p>git</p>
+            </div>
+            <div className='iconDiv'>
+                <i className='devicon-github-plain icon'></i>
+                <p>github</p>
+            </div>
+            <div className='iconDiv'>
+                <i className='devicon-html5-plain icon'></i>
+                <p>html</p>
+            </div>
+            <div className='iconDiv'>
+                <i className='devicon-nodejs-plain icon'></i>
+                <p>nodejs</p>
+            </div>
+            <div className='iconDiv'>
+                <i className='devicon-postgresql-plain icon'></i>
+                <p>postgres</p>
+            </div>
+            <div className='iconDiv'>
+                <i className='devicon-react-original icon'></i>
+                <p>react</p>
+            </div>
+            <div className='iconDiv'>
+                <i className='devicon-visualstudio-plain icon'></i>
+                <p>visual studio</p>
+            </div>
+            <div className='iconDiv'>
+                <i className='devicon-trello-plain icon'></i>
+                <p>trello</p>
+            </div>
         </div>
     )
 }
