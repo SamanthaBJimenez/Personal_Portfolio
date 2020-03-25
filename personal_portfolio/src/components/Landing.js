08fba6f9541197d073aa5f20a6d518c4fc26cb66
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-03-24 23:29:08 -0400

    add content to landing.js and landing.css

--- personal_portfolio/src/components/Landing.js
+++ personal_portfolio/src/components/Landing.js
@@ -4,7 +4,9 @@ import '../css/Landing.css';
 const Landing = () => {
     return (
         <div className='LandingDiv'>
-            
+            <h1>Hi.</h1>
+            <h1>My name is Samantha Jimenez.</h1>
+            <h1>I'm a Fullstack Web Developer.</h1>
         </div>
     )
 }
