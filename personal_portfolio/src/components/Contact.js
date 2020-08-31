a1510b968b1fde45ed9071f63620d82eb3c4940e
Samantha Jimenez <samanthjimenez@pursuit.org>
2020-08-31 07:31:20 -0400

    add toastify to send email button

--- personal_portfolio/src/components/Contact.js
+++ personal_portfolio/src/components/Contact.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
+import { ToastContainer, toast, Flip } from 'react-toastify';
+import './../css/Toast.css';
 
 const Contact = () => {
+    
+    const notify = () => toast.success('Sent!', {
+        position: "bottom-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });;
 
     const handleSubmit = (e) => {
         e.preventDefault();
         emailjs.sendForm('gmail', 'template_o8FUfYii', e.target, 'user_O3tRVjUa8qiKKAuGdRJBS')
             .then((result) => {
                 console.log(result.text);
+                notify();
             }, (error) => {
                 console.log(error.text);
             });
@@ -26,18 +39,18 @@ const Contact = () => {
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label for="name">Your Name</label>
-                                <input type="text" name="name" class="form-control" id="name"/>
+                                <input type="text" name="name" class="form-control" id="name" required/>
                                 <div class="validate"></div>
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="name">Your Email</label>
-                                <input type="email" class="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email"/>
+                                <input type="email" class="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email" required/>
                                 <div class="validate"></div>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="name">Message</label>
-                            <textarea class="form-control" id="message" name="message" rows="10" data-rule="required" data-msg="Please write something for us"></textarea>
+                            <textarea class="form-control" id="message" name="message" rows="10" data-rule="required" data-msg="Please write something for us" required></textarea>
                             <div class="validate"></div>
                         </div>
                         <div class="mb-3">
@@ -45,8 +58,24 @@ const Contact = () => {
                             <div class="error-message"></div>
                             <div class="sent-message">Your message has been sent. Thank you!</div>
                         </div>
-                        <div class="text-center"><button type="submit">Send Message</button></div>
+                        <div class="text-center">
+                        <button type="submit">Send Message</button>
+                        </div>
                     </form>
+                    <ToastContainer
+                        class="toast"
+                        position="bottom-right"
+                        autoClose={4000}
+                        hideProgressBar={false}
+                        transition={Flip}
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                        // style={{"width": "240px", "border-radius": "5px"}}
+                    />
                 </div>
             </div>
         </div>
