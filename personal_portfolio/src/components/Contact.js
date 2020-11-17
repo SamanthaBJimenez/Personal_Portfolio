1b8ac7cbe4cbe5c9923af0e95cbe737f43bbfadc
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-11-17 13:36:20 -0500

    adjust contact component size

--- personal_portfolio/src/components/Contact.js
+++ personal_portfolio/src/components/Contact.js
@@ -34,7 +34,7 @@ const Contact = () => {
                 <p>Feel free to reach out. Let's connect!</p>
             </div>
             <div class="row aos-init aos-animate" data-aos="fade-in">
-                <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+                <div class="col-lg-7 mt-5 mt-lg-0 d-flex-i align-items-stretch">
                     <form onSubmit={handleSubmit} class="php-email-form">
                         <div class="form-row">
                             <div class="form-group col-md-6">
